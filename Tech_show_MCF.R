3e88d7be5c47984d4ac387ca563ba314fde4052d
Siddick1 <55353396+Siddick1@users.noreply.github.com>
2019-12-13 19:34:03 +0200

    Update Tech_show_MCF.R

--- Tech_show_MCF.R
+++ Tech_show_MCF.R
@@ -1,21 +1,10 @@
 # This function is going to help us get all in built data
 data()
 # This the dataset that we will be working with it
-pressure
-data(pressure)
-# To see the corallation between the temperature and the pressure
-plot(temperature~pressure, data=pressure)
-# To calculate the mean
-mean(pressure$temperature)
-mean.temperature=mean(pressure$temperature)
-# Drawing the horizontal line
-abline(h= mean(pressure$temperature),col='green')
-# Using the linear regression
-mod=lm(temperature~pressure, data=pressure)
-mod
-# Drawing the mean in the linear regression model with a red color
-abline(mod,col='red')
+airquality
 # The different graphs
-plot(mod)
+mod=lm(Temp ~ Wind + Ozone,airquality)
+#plot(lm(Temp ~ Wind + Ozone,airquality))
+#plot(lm(Temp ~ Wind + Ozone,airquality))
 # The summary of the linear regression
-summary(mod)
+summary(lm(Temp ~ Wind + Ozone,airquality))
